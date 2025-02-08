47c0ed7085d0d59f82ca20a7a2d30181158948cf
Pratham <prathu032@gmail.com>
2025-02-08 07:28:20 +0000

    making counter function

--- src/counter_project_backend/src/lib.rs
+++ src/counter_project_backend/src/lib.rs
@@ -1,4 +1,31 @@
-#[ic_cdk::query]
-fn greet(name: String) -> String {
-    format!("Hello, {}!", name)
+use ic_cdk_macros::{query, update};
+use std::cell::RefCell;
+
+thread_local! {
+    static COUNTER: RefCell<u64> = RefCell::new(0);
+}
+
+#[query]
+fn get_counter() -> u64 {
+    COUNTER.with(|counter| *counter.borrow())
+}
+
+#[update]
+fn increment_counter() {
+    COUNTER.with(|counter| *counter.borrow_mut() += 1);
+}
+
+#[update]
+fn decrement_counter() {
+    COUNTER.with(|counter| {
+        let mut value = counter.borrow_mut();
+        if *value > 0 {
+            *value -= 1;
+        }
+    });
+}
+
+#[update]
+fn reset_counter() {
+    COUNTER.with(|counter| *counter.borrow_mut() = 0);
 }
